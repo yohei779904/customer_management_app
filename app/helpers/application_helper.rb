28698c063d90356ef73c356b6e47afbd94c5318a
yohei.takei <yohei@hotmail.co.jp>
2020-03-29 12:28:13 +0900

    finished_assets_of_precompile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def document_title
-    if @title.present?
-      '#{@title} - customer_management'
-    else
-      'customer_management'
-    end
+      if @title.present?
+        "#{@title} - customer_management"
+      else
+        "customer_management"
+      end
   end
 end
